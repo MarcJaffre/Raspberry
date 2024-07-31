8c50c97f890adf540cf094cb671d5933ca6fd900
Marc <teste74@hotmail.fr>
2024-07-31 17:15:24 +0200

    Update script2.sh

--- scripts/VEEAM/script2.sh
+++ scripts/VEEAM/script2.sh
@@ -15,6 +15,7 @@ HOST_PASSWORD="admin"
 HOST_SHARE="Media_5/TEST"
 HOST_MOUNTPOINT="/mnt/backup"
 
+
 #################################################################################################################################################
 # Verification #
 ################
@@ -31,37 +32,37 @@ HOST_MOUNTPOINT="/mnt/backup"
 # Menu 0  Adresse du Serveur de partage #
 #########################################
 func_SRV_ADRESS()  {
- read -p "Quel est l'adresse IP du serveur ? " HOST_SERVEUR HOST_SERVEUR=${HOST_SERVEUR:-192.168.20.3}
+ read -p "Quel est l'adresse IP du serveur ? " HOST_SERVEUR; HOST_SERVEUR=${HOST_SERVEUR:-192.168.20.3}
 }
 #################################################################################################################################################
 # Menu 1 - Nom du domaine #
 ###########################
 func_SRV_DOMAINE()  {
- read -p "Quel est le nom de domaine ? " HOST_DOMAINE HOST_DOMAINE=${HOST_DOMAINE:-Local}
+ read -p "Quel est le nom de domaine ? " HOST_DOMAINE; HOST_DOMAINE=${HOST_DOMAINE:-Local}
 }
 #################################################################################################################################################
 # Menu 2 - Nom d'utilisateur #
 ##############################
 func_SRV_USERNAME() {
- read -p "Quel est le nom de compte utilisateur ? " HOST_USERNAME HOST_USERNAME=${HOST_USERNAME:-marc}
+ read -p "Quel est le nom de compte utilisateur ? " HOST_USERNAME; HOST_USERNAME=${HOST_USERNAME:-marc}
 }
 #################################################################################################################################################
 # Menu 3 - Mot de passe #
 #########################
 func_SRV_PASSWORD() {
- read -p "Quel est le mot de passe du compte utilisateur ? " HOST_PASSWORD HOST_PASSWORD=${HOST_PASSWORD:-admin}
+ read -p "Quel est le mot de passe du compte utilisateur ? " HOST_PASSWORD; HOST_PASSWORD=${HOST_PASSWORD:-admin}
 }
 #################################################################################################################################################
 # Menu 4 - Nom du partage #
 ###########################
 func_SRV_PARTAGE()  {
- read -p "Quel est le nom de partage du serveur ? " HOST_SHARE HOST_SHARE=${HOST_SHARE:-marc}
+ read -p "Quel est le nom de partage du serveur ? " HOST_SHARE; HOST_SHARE=${HOST_SHARE:-Media_5/TEST}
 }
 #################################################################################################################################################
 # Menu 5 - Point de montage #
 #############################
 func_HOST_MOUNT1()   {
- read -p "Où souhaitez vous monter le partage sur la machine ? " HOST_MOUNTPOINT HOST_MOUNTPOINT=${HOST_MOUNTPOINT:-/mnt/backup}
+ read -p "Où souhaitez vous monter le partage sur la machine ? " HOST_MOUNTPOINT; HOST_MOUNTPOINT=${HOST_MOUNTPOINT:-/mnt/backup}
 }
 #################################################################################################################################################
 # Menu 6 - Creation du dossier de montage #
@@ -79,15 +80,32 @@ func_HOST_MKDIR()    {
  # Pause
  read -p "";
 }
+
+
 #################################################################################################################################################
 # Menu 7 - Demontage du partage #
 #################################
 func_HOST_UMOUNT()   {
+ # Verification si la variable est NULL,un message est envoyé
+ if [ -z $HOST_SERVEUR     ];then echo "La Valeur Serveur NULL"; fi
+ if [ -z $HOST_MOUNTPOINT  ];then echo "La Valeur Point de montage NULL"; fi
+ #
+ # Demontage
+ if [ ! -z "${HOST_SERVEUR}" ] && [ ! -z "${HOST_MOUNTPOINT}" ];then umount $HOST_MOUNTPOINT; fi
+ #
+ # Pause
+ read -p "";
+}
+
+#################################################################################################################################################
+# Menu 8 - Montage du partage (Local) #
+#######################################
+func_HOST_MOUNT2()  {
  # En cas de variable Vide, un message est envoyé
  if [ -z $HOST_SERVEUR     ];then echo "La Valeur Serveur NULL"; fi
  if [ -z $HOST_MOUNTPOINT  ];then echo "La Valeur Point de montage NULL"; fi
  #
- # Creation de Variable ponctuellement (Utile lors du montage)
+ # Creation de Variable ponctuellement pour la suite des commandes
  OPTION="username=$HOST_USERNAME,password=$HOST_PASSWORD"
  SOURCE="//$HOST_SERVEUR/$HOST_SHARE"
  DESTINATION="$HOST_MOUNTPOINT"
@@ -99,10 +117,15 @@ func_HOST_UMOUNT()   {
     echo "> Montage du Lecteur réseau terminé";
  fi
  #
+ # Remise a zéro des variables
+ OPTION=""
+ SOURCE=""
+ DESTINATION=""
  # Pause
  read -p "";
 }
-####################################################################################################
+
+#################################################################################################################################################
 # Menu 8 - Montage du partage (AD) #
 ####################################
 func_HOST_MOUNT2_AD()  {
@@ -122,11 +145,16 @@ func_HOST_MOUNT2_AD()  {
     echo "> Montage du Lecteur réseau terminé";
  fi
  #
+ # Remise a zéro des variables
+ OPTION=""
+ SOURCE=""
+ DESTINATION=""
+ #
  # Pause
  read -p "";
 }
 
-####################################################################################################
+#################################################################################################################################################
 # Menu 9 - Verification du montage #
 ####################################
 func_HOST_CHECKMOUNT(){
@@ -139,7 +167,7 @@ func_HOST_CHECKMOUNT(){
  read -p "";
 }
 
-####################################################################################################
+#################################################################################################################################################
 # Menu R #
 ##########
 func_RECAP()         {
@@ -158,14 +186,14 @@ func_RECAP()         {
  echo "# ====================================================== #";
 }
 
-####################################################################################################
+#################################################################################################################################################
 # Menu A - Editer le fichier des chemins à sauvegarder #
 ########################################################
 func_HOST_EDIT_RSYNC_FILE(){
  nano $HOME/rsync.txt;
 }
 
-####################################################################################################
+#################################################################################################################################################
 # Menu B - Vérification des chemins #
 #####################################
 func_HOST_CHECK_RSYNC_FOLDER(){
@@ -183,3 +211,211 @@ func_HOST_CHECK_RSYNC_FOLDER(){
  # Pause
  read -p "";
 }
+#################################################################################################################################################
+# Menu C - Lancer la sauvegarde #
+#################################
+func_HOST_ARCHIVAGE_RSYNC(){
+ read -p "";
+}
+#################################################################################################################################################
+# Contenu du Menu #
+####################
+func_MENU()          {
+echo "############################################################"
+echo "#          Information pour le montage du partage          #"
+echo "############################################################"
+echo "Menu 0: Adresse du Serveur de partage ($HOST_SERVEUR)"
+echo "Menu 1: Nom du domaine ($HOST_DOMAINE)"
+echo "Menu 2: Nom d'utilisateur ($HOST_USERNAME)"
+echo "Menu 3: Mot de passe ($HOST_PASSWORD)"
+echo "Menu 4: Nom du partage ($HOST_SHARE)"
+echo
+echo "############################################################"
+echo "#          Gestion du montage sur la machine HOTE          #"
+echo "############################################################"
+echo "Menu 5: Point de montage ($HOST_MOUNTPOINT)"
+echo "Menu 6: Creation du dossier de montage"
+echo "Menu 7: Demontage du partage"
+echo "Menu 8: Montage du partage"
+echo "Menu 9: Verification du montage"
+echo
+echo "############################################################"
+echo "                     En Developpement                      #"
+echo "############################################################"
+echo "Menu A: Editer le fichier des chemins à sauvegarder"
+echo "Menu B: Vérification des chemins"
+echo "Menu C: Lancer la sauvegarde"
+echo "Menu D: "
+echo "Menu E: Tuer Rsync (Urgence)"
+echo "Menu I: Information sur le script"
+echo
+echo "############################################################"
+echo "#          Actions Spécial sur le système                  #"
+echo "############################################################"
+echo "Menu R: Résumer des actions"
+echo "Menu Q: Quitter le menu"
+echo "############################################################"
+echo
+read -p "Indiquer votre choix: " choix
+echo
+}
+#################################################################################################################################################
+# Action du menu #
+##################
+func_CHOIX(){
+case $choix in
+ # ------------------------------------------------------------ #
+ 0)
+  func_SRV_ADRESS;
+  clear;
+ ;;
+ # ------------------------------------------------------------ #
+ 1)
+  func_SRV_DOMAINE; clear;
+ ;;
+ # ------------------------------------------------------------ #
+ 2)
+  func_SRV_USERNAME; clear;
+ ;;
+ # ------------------------------------------------------------ #
+ 3)
+  func_SRV_PASSWORD; clear;
+ ;;
+ # ------------------------------------------------------------ #
+ 4)
+  func_SRV_PARTAGE; clear;
+ ;;
+ # ------------------------------------------------------------ #
+ 5)
+  func_HOST_MOUNT1; clear;
+ ;;
+ # ------------------------------------------------------------ #
+ 6)
+  func_HOST_MKDIR; clear;
+ ;;
+ # ------------------------------------------------------------ #
+ 7)
+  func_HOST_UMOUNT; clear;
+ ;;
+ # ------------------------------------------------------------ #
+ 8)
+  func_HOST_MOUNT2; clear;
+ ;;
+ # ------------------------------------------------------------ #
+ 9)
+  func_HOST_CHECKMOUNT; clear;
+ ;;
+ # ------------------------------------------------------------ #
+ a|A)
+  echo
+  func_HOST_EDIT_RSYNC_FILE; clear;
+ ;;
+ # ------------------------------------------------------------ #
+ b|B)
+  func_HOST_CHECK_RSYNC_FOLDER; clear;
+ ;;
+ # ------------------------------------------------------------ #
+ c|C)
+  func_HOST_ARCHIVAGE_RSYNC; clear;
+ ;;
+ # ------------------------------------------------------------ #
+ d|D)
+  echo
+  echo    "#-------------------------#"
+  echo    "# Bienvenue sur le menu D #"
+  echo    "#-------------------------#"
+  read -p ""
+  clear;
+ ;;
+ # ------------------------------------------------------------ #
+ e|E)
+  echo
+  echo    "#-------------------------#"
+  echo    "# Bienvenue sur le menu E #"
+  echo    "#-------------------------#"
+  read -p ""
+  clear;
+ ;;
+ # ------------------------------------------------------------ #
+ f|F)
+  echo
+  echo    "#-------------------------#"
+  echo    "# Bienvenue sur le menu F #"
+  echo    "#-------------------------#"
+  read -p ""
+  clear;
+ ;;
+ # ------------------------------------------------------------ #
+ g|G)
+  echo
+  echo    "#-------------------------#"
+  echo    "# Bienvenue sur le menu G #"
+  echo    "#-------------------------#"
+  read -p ""
+  clear;
+ ;;
+ # ------------------------------------------------------------ #
+ h|H)
+  echo
+  echo    "#-------------------------#"
+  echo    "# Bienvenue sur le menu H #"
+  echo    "#-------------------------#"
+  read -p ""
+  clear;
+ ;;
+ # ------------------------------------------------------------ #
+ i|I)
+  echo
+  echo    "#-------------------------#"
+  echo    "# Bienvenue sur le menu I #"
+  echo    "#-------------------------#"
+  read -p ""
+  clear;
+ ;;
+ # ------------------------------------------------------------ #
+ r|R)
+  func_RECAP;
+  echo
+  echo    "#-------------------------#"
+  echo    "# Bienvenue sur le menu R #"
+  echo    "#-------------------------#"
+  read -p ""
+  clear;
+ ;;
+ # ------------------------------------------------------------ #
+ q|Q)
+  clear; exit;
+ ;;
+ # ------------------------------------------------------------ #
+ *)
+  echo
+  echo    "#--------------------------------------------------#"
+  echo    "# Merci d'indiquer un choix parmis ceux disponible #"
+  read -p "#--------------------------------------------------#"
+  clear;
+ ;;
+ # ------------------------------------------------------------ #
+ esac
+}
+
+#################################################################################################################################################
+# Nettoyage de la console #
+###########################
+clear;
+
+#################################################################################################################################################
+# Récupération de l'ID en cours #
+#################################
+ID=$(id -u)
+
+#################################################################################################################################################
+# Vérification Root #
+#####################
+if [[ $ID = 0 ]];then MENU=0; else echo "Veuiller lancer le script depuis root"; MENU=1; fi
+
+####################################################################################################
+# Boucle Infinie #
+##################
+# Si le compte est root ($MENU) alors lancer la fonction.
+while [ $MENU = 0 ];do func_MENU; func_CHOIX; done
+####################################################################################################
