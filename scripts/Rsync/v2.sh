9a23cd13da66b3f2670cfa38bfeebc1fe748d9f1
Marc <teste74@hotmail.fr>
2024-08-01 01:12:07 +0200

    Update v2.sh

--- scripts/Rsync/v2.sh
+++ scripts/Rsync/v2.sh
@@ -268,14 +268,14 @@ func_HOST_ARCHIVAGE_RSYNC(){
             echo "# Lancement   : $(date +"%d/%m/%y à %H:%M")                                                                               "
             echo "# Fichier log : $HOST_MOUNTPOINT/Journal/$DATE.log;                                                                       "
             echo "# Dossier     : $i                                                                                                        "
-            echo "# Taille      : $(du -h -d0 . | cut -d "." -f 1-2)                                                                        "
+            echo "# Taille      : $(du -h -d0 $i | cut -d "." -f 1)                                                                         "
             ########################################################################################################################################################################################
             echo "##########################################################################################################################" >> $HOST_MOUNTPOINT/Journal/$DATE.log;
-            echo "# Debut de l'archivage : $(date +"%d%m%y_%H%M%S") pour le dossier $i                                                      " >> $HOST_MOUNTPOINT/Journal/$DATE.log;
+            echo "# Debut de l'archivage : $(date +"%d/%m/%y à %H:%M") pour le dossier $i                                                   " >> $HOST_MOUNTPOINT/Journal/$DATE.log;
             echo ""                                                                                                                           >> $HOST_MOUNTPOINT/Journal/$DATE.log;
             rsync -avz $i $HOST_MOUNTPOINT                                                                                                    >> $HOST_MOUNTPOINT/Journal/$DATE.log;
             echo ""                                                                                                                           >> $HOST_MOUNTPOINT/Journal/$DATE.log;
-            echo "# Fin de l'archivage : $(date +"%d%m%y_%H%M%S") pour le dossier $i                                                        " >> $HOST_MOUNTPOINT/Journal/$DATE.log;
+            echo "# Fin de l'archivage : $(date +"%d/%m/%y à %H:%M") pour le dossier $i                                                     " >> $HOST_MOUNTPOINT/Journal/$DATE.log;
             echo "##########################################################################################################################" >> $HOST_MOUNTPOINT/Journal/$DATE.log;
             echo ""                                                                                                                           >> $HOST_MOUNTPOINT/Journal/$DATE.log;
             echo ""                                                                                                                           >> $HOST_MOUNTPOINT/Journal/$DATE.log;
