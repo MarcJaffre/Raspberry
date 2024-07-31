e9dd890b3464e72f41e46f72ccbb9f24ecceb416
Marc <teste74@hotmail.fr>
2024-07-31 16:57:06 +0200

    Update script2.sh

--- scripts/VEEAM/script2.sh
+++ scripts/VEEAM/script2.sh
@@ -24,56 +24,56 @@ HOST_MOUNTPOINT="/mnt/backup"
 #if [ -z $HOST_USERNAME    ];then echo "La Valeur USERNAME NULL"; fi
 #if [ -z $HOST_SHARE       ];then echo "La Valeur Partage NULL"; fi
 #if [ -z $HOST_MOUNTPOINT  ];then echo "La Valeur Point de montage NULL"; fi
+#if [ ! -f $HOME/rsync.txt ];then echo "Le fichier rsync.txt est absent"; fi
 #if [ ! -s $HOME/rsync.txt ];then echo "Le fichier rsync.txt est vide"; fi
-
 
 #################################################################################################################################################
 # Menu 0  Adresse du Serveur de partage #
 #########################################
 func_SRV_ADRESS()  {
-  read -p "Quel est l'adresse IP du serveur ? " HOST_SERVEUR HOST_SERVEUR=${HOST_SERVEUR:-192.168.20.3}
+ read -p "Quel est l'adresse IP du serveur ? " HOST_SERVEUR HOST_SERVEUR=${HOST_SERVEUR:-192.168.20.3}
 }
 #################################################################################################################################################
 # Menu 1 - Nom du domaine #
 ###########################
 func_SRV_DOMAINE()  {
-  read -p "Quel est le nom de domaine ? " HOST_DOMAINE HOST_DOMAINE=${HOST_DOMAINE:-Local}
+ read -p "Quel est le nom de domaine ? " HOST_DOMAINE HOST_DOMAINE=${HOST_DOMAINE:-Local}
 }
 #################################################################################################################################################
 # Menu 2 - Nom d'utilisateur #
 ##############################
 func_SRV_USERNAME() {
-  read -p "Quel est le nom de compte utilisateur ? " HOST_USERNAME HOST_USERNAME=${HOST_USERNAME:-marc}
+ read -p "Quel est le nom de compte utilisateur ? " HOST_USERNAME HOST_USERNAME=${HOST_USERNAME:-marc}
 }
 #################################################################################################################################################
 # Menu 3 - Mot de passe #
 #########################
 func_SRV_PASSWORD() {
-  read -p "Quel est le mot de passe du compte utilisateur ? " HOST_PASSWORD HOST_PASSWORD=${HOST_PASSWORD:-admin}
+ read -p "Quel est le mot de passe du compte utilisateur ? " HOST_PASSWORD HOST_PASSWORD=${HOST_PASSWORD:-admin}
 }
 #################################################################################################################################################
 # Menu 4 - Nom du partage #
 ###########################
 func_SRV_PARTAGE()  {
-  read -p "Quel est le nom de partage du serveur ? " HOST_SHARE HOST_SHARE=${HOST_SHARE:-marc}
+ read -p "Quel est le nom de partage du serveur ? " HOST_SHARE HOST_SHARE=${HOST_SHARE:-marc}
 }
 #################################################################################################################################################
 # Menu 5 - Point de montage #
 #############################
 func_HOST_MOUNT1()   {
-  read -p "Où souhaitez vous monter le partage sur la machine ? " HOST_MOUNTPOINT HOST_MOUNTPOINT=${HOST_MOUNTPOINT:-/mnt/backup}
+ read -p "Où souhaitez vous monter le partage sur la machine ? " HOST_MOUNTPOINT HOST_MOUNTPOINT=${HOST_MOUNTPOINT:-/mnt/backup}
 }
 #################################################################################################################################################
 # Menu 6 - Creation du dossier de montage #
 ###########################################
 func_HOST_MKDIR()    {
- # Verification si la variable est NULL
+ # Verification si la variable est NULL,un message est envoyé
  if [ -z $HOST_MOUNTPOINT   ];then echo  "La Valeur Point de montage NULL"; fi
  #
- # Si le point de montage existe deja
+ # Si le point de montage existe deja,un message est envoyé
  if [ -d $HOST_MOUNTPOINT   ];then echo  "> Le dossier de montage existe déjà."; fi
  #
- # Si le point de montage n'existe pas
+ # Si le point de montage n'existe pas,un message est envoyé
  if [ ! -d $HOST_MOUNTPOINT ];then mkdir -p $HOST_MOUNTPOINT; echo "> Le dossier de montage a ete cree."; fi
  #
  # Pause
@@ -83,11 +83,11 @@ func_HOST_MKDIR()    {
 # Menu 7 - Demontage du partage #
 #################################
 func_HOST_UMOUNT()   {
- # En cas de variable Vide , message retour
+ # En cas de variable Vide, un message est envoyé
  if [ -z $HOST_SERVEUR     ];then echo "La Valeur Serveur NULL"; fi
  if [ -z $HOST_MOUNTPOINT  ];then echo "La Valeur Point de montage NULL"; fi
  #
- # Creation de Variable ponctuellement (Utile lors du montage
+ # Creation de Variable ponctuellement (Utile lors du montage)
  OPTION="username=$HOST_USERNAME,password=$HOST_PASSWORD"
  SOURCE="//$HOST_SERVEUR/$HOST_SHARE"
  DESTINATION="$HOST_MOUNTPOINT"
@@ -106,7 +106,7 @@ func_HOST_UMOUNT()   {
 # Menu 8 - Montage du partage (AD) #
 ####################################
 func_HOST_MOUNT2_AD()  {
- # En cas de variable Vide, message retour
+ # En cas de variable Vide, un message est envoyé
  if [ -z $HOST_SERVEUR     ];then echo "La Valeur Serveur NULL"; fi
  if [ -z $HOST_MOUNTPOINT  ];then echo "La Valeur Point de montage NULL"; fi
  #
@@ -130,7 +130,7 @@ func_HOST_MOUNT2_AD()  {
 # Menu 9 - Verification du montage #
 ####################################
 func_HOST_CHECKMOUNT(){
- # En cas de variable Vide, message retour
+ # En cas de variable Vide, un message est envoyé
  if [ -z $HOST_MOUNTPOINT  ];then echo "La Valeur Point de montage NULL"; fi
  #
  # Si la variable est pas null, verification du montage
@@ -162,5 +162,24 @@ func_RECAP()         {
 # Menu A - Editer le fichier des chemins à sauvegarder #
 ########################################################
 func_HOST_EDIT_RSYNC_FILE(){
-  nano $HOME/rsync.txt;
+ nano $HOME/rsync.txt;
+}
+
+####################################################################################################
+# Menu B - Vérification des chemins #
+#####################################
+func_HOST_CHECK_RSYNC_FOLDER(){
+ #
+ # Verifier si le fichier n'existe pas, un message est envoyé
+ if [ ! -f $HOME/rsync.txt ];then echo "Le fichier rsync.txt est absent"; fi
+ #
+ # Verifier si le fichier est pas vide
+ if [ ! -s $HOME/rsync.txt ];then echo "Le fichier rsync.txt est vide"; fi
+ #
+ # Lecture du fichier rsync, verification de repertoire. (Lecteur ligne par ligne)
+ for i in $(cat $HOME/rsync.txt);do if [ -d $i   ];then echo "[OK] Le répertoire existe : $i"; fi done
+ for i in $(cat $HOME/rsync.txt);do if [ ! -d $i ];then echo "[KO] Le répertoire n'existe pas : $i"; fi done
+ #
+ # Pause
+ read -p "";
 }
