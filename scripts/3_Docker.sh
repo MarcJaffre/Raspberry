fcbd4bc20178bc748d35bd0399bdb8215473375f
Marc <teste74@hotmail.fr>
2024-08-18 06:31:35 +0200

    Update 3_Docker.sh

--- scripts/3_Docker.sh
+++ scripts/3_Docker.sh
@@ -18,10 +18,10 @@ func_question(){
 
 ##################################################################################################################################
 func_check_datastore(){
-if [ ! -d $DATASTORE ];then
+ if [ ! -d $DATASTORE ];then
   mkdir $DATASTORE 2>/dev/null;
   echo "Creation du dossier";
- fi
+ fi;
 }
 
 ##################################################################################################################################
@@ -29,3 +29,24 @@ func_get_container_running(){
  docker ps --format '{{.Names}}' | sort -n | xargs -n1 > /tmp/running;
 }
 
+##################################################################################################################################
+func_stop_container(){
+ for i in $(cat /tmp/running);do
+  docker stop $i 1>/dev/null;
+ done;
+}
+
+##################################################################################################################################
+func_start_container(){
+ for i in $(cat /tmp/running);do
+  docker start $i 1>/dev/null 2>/dev/null;
+ done;
+}
+
+func_volumes_backup(){
+for VOLUME in $(ls /var/lib/docker/volumes | sort -n | grep -v "backingFsBlockDev\|metadata.db");do
+docker-volume-snapshot create "$VOLUME" "$DATASTORE"/"$VOLUME".tar;
+done
+}
+
+func_volumes_backup
