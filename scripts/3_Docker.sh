8b83bc30f1f94e9d8965469db889170fbed834f5
Marc <teste74@hotmail.fr>
2024-08-18 06:45:57 +0200

    Update 3_Docker.sh

--- scripts/3_Docker.sh
+++ scripts/3_Docker.sh
@@ -11,23 +11,25 @@ clear;
 #source ./settings;
 DATASTORE="/mnt/Media_5/Backup/Docker"
 
+# Blank = "null" ou "echo"
+BLANK="echo"
 ##################################################################################################################################
 func_question(){ read -p "Quel action souhaitre vous réaliser ? (backup | restore) " REPONSE; }
-func_check_datastore(){ if [ ! -d $DATASTORE ];then mkdir $DATASTORE 2>/dev/null; echo "Creation du dossier"; fi; }
+func_check_datastore(){ if [ ! -d $DATASTORE ];then mkdir $DATASTORE 2>/dev/null; $BLANK "Creation du dossier"; fi; }
 func_get_container_running(){ docker ps --format '{{.Names}}' | sort -n | xargs -n1 > /tmp/running; }
-func_stop_container(){ for i in $(docker ps -a --format '{{.Names}}' | sort -n | xargs -n1); do echo docker stop $i; done; }
-func_start_container(){ for i in $(cat /tmp/running);do echo docker start $i ; done; }
+func_stop_container(){ for i in $(docker ps -a --format '{{.Names}}' | sort -n | xargs -n1); do $BLANK docker stop $i; done; }
+func_start_container(){ for i in $(cat /tmp/running);do $BLANK docker start $i ; done; }
 # 1>/dev/null 2>/dev/null
 ##################################################################################################################################
 func_volumes_backup(){
  for VOLUME in $(ls /var/lib/docker/volumes | sort -n | grep -v "backingFsBlockDev\|metadata.db");do
-  echo docker-volume-snapshot create "$VOLUME" "$DATASTORE"/"$VOLUME".tar;
+  $BLANK docker-volume-snapshot create "$VOLUME" "$DATASTORE"/"$VOLUME".tar;
  done
 }
 
 func_volumes_restore(){
  for VOLUME in $(ls $DATASTORE | cut -d "." -f1 | xargs -n1);do
-  echo docker-volume-snapshot restore   $DATASTORE/$VOLUME.tar "$VOLUME" 
+  $BLANK docker-volume-snapshot restore   $DATASTORE/$VOLUME.tar "$VOLUME" 
  done
 }
 
