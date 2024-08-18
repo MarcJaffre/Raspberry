4df5192c913e16b78bf20ab0353014f6377ef62f
Marc <teste74@hotmail.fr>
2024-08-18 04:05:53 +0200

    Update 9_Docker_resto_vol.sh

--- scripts/9_Docker_resto_vol.sh
+++ scripts/9_Docker_resto_vol.sh
@@ -1,6 +1,5 @@
 clear;
 cat > restore.sh << EOF
-
 #######################################################################################################################
 #!/usr/bin/bash
 
@@ -22,18 +21,31 @@ read -p "Souhaitez vous lancer la restauration ? (o|y) " VALIDATION
 #######################################################################################################################
 # Restauration #
 ################
-if (( \$VALIDATION == y || \$VALIDATION == o ));then
-   for VOLUME in \$(ls \$DATASTORE | cut -d "." -f1 | xargs -n1)
+if (( $VALIDATION == y || $VALIDATION == o ));then
+   ####################################################################################################################
+   # Arret des conteneurs
+   clear;
+   echo "Arrêt des conteneurs pendant la restauration";
+     for i in $(docker ps --format '{{.Names}}');do docker stop $i; done
+   ####################################################################################################################
+   # Restauration des volumes
+   for VOLUME in $(ls $DATASTORE | cut -d "." -f1 | xargs -n1)
    do
     # Actions par volume
-    echo "# --------------------------------------------------------- #";
-    echo "Restauration du volume \$VOLUME en cours";
+     echo "___________________________________________________________________________________________________________"
+    echo "Restauration du volume $VOLUME en cours";
     # ----------------------------------------------------------------------------- #
-    docker-volume-snapshot restore \$DATASTORE/\$VOLUME.tar \$VOLUME 1>/dev/null;
+    docker-volume-snapshot restore $DATASTORE/$VOLUME.tar $VOLUME 1>/dev/null;
     # ----------------------------------------------------------------------------- #
-    echo "Restauration terminée";
+    echo "Restauration du volume $VOLUME terminée";
     echo "";
    done
+   ####################################################################################################################
+   # Relance des conteneurs x2 (Conteneurs dependant de MariaDB)
+   echo "Démarrage des conteneurs.";
+   for i in $(docker ps -a --format '{{.Names}}');do docker start $i 2>/dev/null; done
+   for i in $(docker ps -a --format '{{.Names}}');do docker start $i; done
+   ####################################################################################################################   
 fi
 #######################################################################################################################
 EOF
