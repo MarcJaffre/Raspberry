73668c3efe454dc2cfd36412600f733ad079dab0
Marc <teste74@hotmail.fr>
2024-08-18 04:46:39 +0200

    Update 3_Docker.sh

--- scripts/3_Docker.sh
+++ scripts/3_Docker.sh
@@ -14,7 +14,7 @@ source ./settings;
 # FONCTIONS #
 #############
 func_question(){
- read -p "Quel action souhaitre vous réaliser ? (backup | recovery)";
+ read -p "Quel action souhaitre vous réaliser ? (backup | recovery)" REPONSE;
 }
 
 func_check_datastore(){
@@ -29,3 +29,25 @@ func_docker_stop_container(){
  for i in $CN_RUNNING;do echo "docker stop $i"; done
 }
 
+func_docker_start_container(){
+ for i in $CN_RUNNING;do echo "docker stop $i"; done
+}
+
+func_backup(){
+if [ $REPONSE = backup ];then
+ echo "Backup";
+fi
+}
+
+func_restore(){
+if [ $REPONSE = restore ];then
+ echo "Restore";
+fi
+}
+
+func_check_datastore;
+func_get_container_running;
+func_docker_stop_container;
+func_backup;
+func_restore;
+func_docker_start_container;
