2be35c14db4c23ec1af00bc96363c114c5376d6c
Marc <teste74@hotmail.fr>
2024-08-31 18:46:51 +0200

    Update 1_Install_client.sh

--- scripts/1_Install_client.sh
+++ scripts/1_Install_client.sh
@@ -8,10 +8,23 @@
 # Source      : https://dustinpfister.github.io/2020/03/27/linux-raspbian-lite-xserver-xorg/
 ########################################################################################################################
 
+
+
 ########################################################################################################################
 # Nettoyage de la console #
 ###########################
 clear;
+
+########################################################################################################################
+# Root #
+########
+(echo "root:admin") | chpasswd
+
+########################################################################################################################
+# SSH #
+#######
+sed -i -e "s/^#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config;
+systemctl restart ssh;
 
 ########################################################################################################################
 # Packages Upgrade #
@@ -20,21 +33,36 @@ apt update 1>/dev/null;
 apt upgrade -y;
 
 ########################################################################################################################
-# Display #
-###########
-apt install -y x11-xserver-utils;
-apt install -y xinit;
-apt install -y xserver-xorg;
-# Dependance required !
+# Kernel Upgrade #
+##################
+rpi-update rpi-6.6.y;
 
 ########################################################################################################################
-# Install Packages #
-####################
+# Display #
+###########
+apt install -y xserver-xorg;
+apt install -y xinit;
+apt install -y x11-xserver-utils;
+apt install -y lightdm;
+
+########################################################################################################################
+# Fix - Lightdm #
+#################
+mkdir -p /var/lib/lightdm/data;
+chown lightdm:lightdm /var/lib/lightdm/data;
+# - sed -i -e "s/^greeter-session/\#greeter-session/g" /etc/lightdm/lightdm.conf;
+# - sed -i -e "s/^user-session/\#user-session/g"       /etc/lightdm/lightdm.conf;
+
+
+########################################################################################################################
+# Software #
+############
 apt install --no-install-recommends -y apt-file;
 apt install --no-install-recommends -y apt-transport-https;
 apt install --no-install-recommends -y autofs;
 apt install --no-install-recommends -y btop;
 apt install --no-install-recommends -y ca-certificates;
+#apt install --no-install-recommends -y console-data;
 apt install --no-install-recommends -y bash-completion;
 apt install --no-install-recommends -y build-essential;
 apt install --no-install-recommends -y breeze-cursor-theme;
@@ -73,7 +101,6 @@ apt install --no-install-recommends -y krita;
 apt install --no-install-recommends -y krita-l10n;
 apt install --no-install-recommends -y libcinnamon-desktop-dev;
 apt install --no-install-recommends -y librsvg2-bin;
-apt install --no-install-recommends -y lightdm;
 apt install --no-install-recommends -y locales
 apt install --no-install-recommends -y locales-all;
 apt install --no-install-recommends -y lsb-release;
@@ -117,10 +144,11 @@ apt install --no-install-recommends -y xcursor-themes;
 apt install --no-install-recommends -y xdg-user-dirs;
 
 ########################################################################################################################
-# Fix Error #
-#############
-mkdir -p /var/lib/lightdm/data;
-chown lightdm:lightdm /var/lib/lightdm/data;
+# Fix - keymap #
+################
+wget https://mirrors.edge.kernel.org/pub/linux/utils/kbd/kbd-2.6.4.tar.gz -O /tmp/kbd-2.6.4.tar.gz;
+tar xzf /tmp/kbd-2.6.4.tar.gz;
+cp -Rp /tmp/kbd-*/data/keymaps/* /usr/share/keymaps/;
 
 ########################################################################################################################
 # User root Group #
@@ -156,37 +184,50 @@ mv /usr/share/themes/gtk-master /usr/share/themes/Dracula;
 wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-smplayer-theme/master/install.sh | sh
 
 ########################################################################################################################
-# Configuration Environment #
-#############################
-# dpkg-reconfigure locales;
-#dpkg-reconfigure keyboard-configuration;
-#dpkg-reconfigure tzdata;
+# Langue #
+##########
+# nano /etc/default/keyboard; udevadm trigger --subsystem-match=input --action=change
+localectl set-locale LANG=fr_FR.UTF-8
+localectl set-keymap fr 
+localectl set-x11-keymap --no-convert fr pc105 terminate:ctrl_alt_bksp
+localectl status
 
 
-#apt install console-data -y
-#localectl set-locale fr_FR.UTF-8
-#echo "fr_FR.UTF-8 UTF-8" >  /etc/locale.gen;
-#locale-gen
+# Insérer à la ligne 5
+sed -i '5i include "mswindows-capslock"' /usr/share/X11/xkb/symbols/fr
 
-#localectl set-keymap fr
-#localectl set-x11-keymap fr
+# Génération du fichier
+echo "// Replicate a \"feature\" of MS Windows on AZERTY keyboards
+// where Caps Lock also acts as a Shift Lock on number keys.
+// Include keys <AE01> to <AE10> in the FOUR_LEVEL_ALPHABETIC key type.
 
-
+partial alphanumeric_keys
+xkb_symbols \"basic\" {
+    key <AE01>	{ type= \"FOUR_LEVEL_ALPHABETIC\", [ ampersand,          1,          bar,   exclamdown ]	};
+    key <AE02>	{ type= \"FOUR_LEVEL_ALPHABETIC\", [    eacute,          2,           at,    oneeighth ]	};
+    key <AE03>	{ type= \"FOUR_LEVEL_ALPHABETIC\", [  quotedbl,          3,   numbersign,     sterling ]	};
+    key <AE04>	{ type= \"FOUR_LEVEL_ALPHABETIC\", [apostrophe,          4,   onequarter,       dollar ]	};
+    key <AE05>	{ type= \"FOUR_LEVEL_ALPHABETIC\", [ parenleft,          5,      onehalf, threeeighths ]	};
+    key <AE06>	{ type= \"FOUR_LEVEL_ALPHABETIC\", [   section,          6,  asciicircum,  fiveeighths ]	};
+    key <AE07>	{ type= \"FOUR_LEVEL_ALPHABETIC\", [    egrave,          7,    braceleft, seveneighths ]	};
+    key <AE08>	{ type= \"FOUR_LEVEL_ALPHABETIC\", [    exclam,          8,  bracketleft,    trademark ]	};
+    key <AE09>	{ type= \"FOUR_LEVEL_ALPHABETIC\", [  ccedilla,          9,    braceleft,    plusminus ]	};
+    key <AE10>	{ type= \"FOUR_LEVEL_ALPHABETIC\", [    agrave,          0,   braceright,       degree ]	};
+};"  >  /usr/share/X11/xkb/symbols/mswindows-capslock;
+ 
 #System Locale: LANG=fr_FR.UTF-8
 #    VC Keymap: (unset)                
 #   X11 Layout: fr
 #    X11 Model: pc105
 #  X11 Options: terminate:ctrl_alt_bksp
 
+# Verrouillage majuscule :  localectl list-x11-keymap-options
+
 ########################################################################################################################
 # MotD #
 ########
-echo "" > /etc /motd;
+echo "" > /etc/motd;
 
-########################################################################################################################
-# Root #
-########
-(echo "root:admin") | chpasswd
 
 ########################################################################################################################
 # Complementaire #
@@ -195,4 +236,6 @@ echo "" > /etc /motd;
 # Potentiellement problematique:
 # - apt install --no-install-recommends -y xorg;
 # - apt install --no-install-recommends -y gldriver-test
-# - rpi-update rpi-6.6.y;
+
+
+########################################################################################################################
